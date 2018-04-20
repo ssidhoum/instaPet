d43f6beddb4dfa718545436fd290d64882cbbf46
sara.sd <sara.sidhoum25@gmail.com>
2018-04-20 15:27:54 +0200

    post entity initialization

---
+++ src/Model/Entity/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+use Cake\Collection\Collection;
+
+class Post extends Entity
+{
+    protected $_accessible = [
+        'name' => true,
+        'content' => true,
+        'created' => true,
+        'user_id' => true,
+        'photo' => true,
+        'photo_dir'=>true
+
+    ];
+
+
+}
